608b4164f85912ef4f0df29e11eccaa62cfd0e7c
Pacome Rousselle <pacomerouselle@gmail.com>
2023-01-18 13:44:43 +0100

    Included base execution

--- launch.sh
+++ launch.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 make
 ./cvt_vid.sh v2r in/input.mp4 in/input.raw
+
+./base in/input.raw out/output.raw
 ./sobel in/input.raw out/output.raw
-./cvt_vid.sh r2v out/output.raw out/output.mp4
-mplayer out/output.mp4 
+
+for file in $(basename -s .raw out/*.raw); do
+./cvt_vid.sh r2v out/$file.raw out/$file.mp4
+done
+
+for plot in $(ls plot/*.gp); do
+gnuplot $plot
+done
+
+mplayer out/output.mp4
+
 make clean
